0e44cb19ab1e511e92b7f79aa2e2b1027f9e01ff
juva-quentin <quentin.juvet@ynov.com>
2023-10-12 11:05:39 +0200

    creat the pixel war component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PixelWarComponent } from './pixel-war/pixel-war.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PixelWarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pixel-war/pixel-war.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pixel-war',
+  templateUrl: './pixel-war.component.html',
+  styleUrls: ['./pixel-war.component.css']
+})
+export class PixelWarComponent {
+
+}
