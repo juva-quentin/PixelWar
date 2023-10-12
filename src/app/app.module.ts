9da90f6ad64ed65492b3c2d522e2a2f1cbb20397
juva-quentin <quentin.juvet@ynov.com>
2023-10-12 16:22:56 +0200

    other feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PixelWarComponent } from './pixel-war/pixel-war.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     PixelWarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+      ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
